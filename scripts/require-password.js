bdb8a97448a80ff40f42967c3fa49db2a7772234
alicia dewitt <aro.dewitt@gmail.com>
2018-04-11 17:23:45 -0400

    moves timeline.html and adds page for selection of illustrations

---
+++ scripts/require-password.js
@@ -0,0 +1,12 @@
+var password;
+var pass1 = "yellow-ochre3";
+
+password = prompt('Enter Password To View Page',' ');
+
+if (password == pass1) {
+  alert('Great, see you inside. Click OK to enter.');
+}
+
+else {
+  window.location="http://www.aliciadewitt.com";
+}
